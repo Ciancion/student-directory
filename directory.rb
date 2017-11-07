0fa0d73283eb11b2ec9b3ff59ad8cfd67267a951
Valentina <valentina.rmo@gmail.com>
2017-11-07 13:07:52 +0000

    Add total number of students

--- directory.rb
+++ directory.rb
@@ -11,3 +11,8 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+
+#finally, we print the total number of students
+print "Overall, we have "
+print 11
+puts "great students"
