aa2df1b93437f16afe0f6ea055e7a52c5c477e00
Valentina <valentina.rmo@gmail.com>
2017-11-09 14:45:24 +0000

    Add cohort Key and conditions on that key

--- directory.rb
+++ directory.rb
@@ -1,25 +1,37 @@
 def input_students
-  puts "Please insert student names"
-  puts "Then insert the nationality, and add age of the students"
+  puts "Please insert student names and cohort"
+  #puts "Then insert the nationality, and add age of the students"
   puts "To finish, just hit retur twice"
   #create an empty array
   students = []
   name = gets.chomp.downcase
 
+
   while !name.empty? do
     #add the student hash to the array
+    cohort = gets.chomp.downcase
+    cohorts = ["january", "february", "march", "april", "may", "september", "october", "november", "december"]
+    if cohort == ""
+      cohort = "november"
+    end
+    while !cohorts.include?(cohort)
+      if cohort == "june" || cohort == "july" || cohort == "august"
+        puts "There are no cohorts in June, July, August. Insert a valid courth"
+          cohort = gets.chomp
+      else
+        puts "There is a typo, please write the cohort again"
+        cohort = gets.chomp
+      end 
+    end
+    puts "Please insert nationality and age"
     nationality = gets.chomp.downcase
     age = gets.chomp.downcase
-
-    if name.length < 12
-      students << {name: name, cohort: :november, nationality: nationality, age: age }
-      puts "We have #{students.count} students"
-    end
-
-    puts "Please enter the names, nationality and age of the students"
+    students << {name: name, cohort: cohort, nationality: nationality, age: age }
+    puts "We have #{students.count} students"
+    puts "Please enter the names and cohort of the students"
     name = gets.chomp.downcase
-   end
-students
+  end
+  students
 end
 
 def print_header
@@ -46,7 +58,7 @@ end
 
 def print_footer(names)
 
-  footer_string = "Overall, we have #{names.count} students whose name is shorter than 12 characters"
+  footer_string = "Overall, we have #{names.count} great students "
   lenght_footer = footer_string.length
   puts footer_string.center(@length + (lenght_footer - @header_l))
 end
