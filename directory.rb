48bf7d6e41bda50e6797ad687e447994f4298883
Valentina <valentina.rmo@gmail.com>
2017-11-09 11:06:24 +0000

    Add extra keys in studends hash

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
 def input_students
-  puts "Please enter the names of the students"
+  puts "Please insert student names"
+  puts "Then insert the nationality, and add age of the students"
   puts "To finish, just hit retur twice"
   #create an empty array
   students = []
   name = gets.chomp.downcase
+
   while !name.empty? do
     #add the student hash to the array
-    if name.length < 12
-      students << {name: name, cohort: :november}
-      puts "We have #{students.count} students"
+    nationality = gets.chomp.downcase
+    age = gets.chomp.downcase
 
+    if name.length < 12
+      students << {name: name, cohort: :november, nationality: nationality, age: age }
+      puts "We have #{students.count} students"
     end
+
+    puts "Please enter the names, nationality and age of the students"
     name = gets.chomp.downcase
-  end
-  students
+   end
+students
 end
 
 def print_header
@@ -22,15 +28,15 @@ def print_header
 end
 
 def print(names)
-  # names.each do |student|
-index = 0
-#l = (names.length - 1).to_int
- while (names.length - 1) > index
 
-   puts "#{index}." + "#{names[index][:name]}" + " (#{names[index][:cohort]} cohort) "
+index = 0
+
+ while (names.length) > index
+
+   puts "#{index}." + "#{names[index][:name]}," + " (#{names[index][:cohort]} cohort), " + "#{names[index][:nationality]}, " + "#{names[index][:age]} years old"
    index += 1
  end
-  #end
+
 
 end
 
