f5f3c047b8153e6ba45c3f6ac7bceecc1adae90b
Valentina <valentina.rmo@gmail.com>
2017-11-10 22:51:08 +0000

    Add Interactive menu

--- directory.rb
+++ directory.rb
@@ -81,14 +81,36 @@ def print_footer(names)
   puts footer_string.center(@length + (lenght_footer - @header_l))
 end
 
-students = input_students
-if students.count != 0
-
-  print_header
-  print(students)
-  print_footer(students)
-else
-  puts "The list of students is empty!"
-
-
+def interactive_menu
+  students = []
+  loop do
+    #1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    #2. read the input and save it into a variable
+    selection = gets.chomp
+    #3. do what the user has asked
+    case selection
+    when "1"
+      #input student
+      students = input_students
+    when "2"
+      #show the students
+      if students.count != 0
+        print_header
+        print(students)
+        print_footer(students)
+      else
+        puts "The list of students is empty!"
+      end
+    when "9"
+      exit #this will terminate the program
+    else
+      puts "I don\'t lnow what you ment, try again"
+    end
+  end
 end
+
+
+interactive_menu
